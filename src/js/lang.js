694b16e2f097dfa5d600420e1400d6f77bb8b90f
CalleAlexanderson <calle.alexanderson@telia.com>
2024-02-27 19:31:33 +0100

    first commit

---
+++ src/js/lang.js
@@ -0,0 +1,51 @@
+// Globala konstanter och variabler
+let swedishBtn;
+let englishBtn;
+// --------------------------------------------------
+// Initiera globala variabler och händelsehanterare
+function init() {
+
+    swedishBtn = document.getElementsByClassName('langBtn')[0];
+    englishBtn = document.getElementsByClassName('langBtn')[1];
+
+    console.log(swedishBtn);
+    console.log(englishBtn);
+
+    swedishBtn.addEventListener('click', makeLangSwedish);
+    englishBtn.addEventListener('click', makeLangEnglish);
+
+} // Slut init
+window.addEventListener('load', init);
+// --------------------------------------------------
+
+function makeLangSwedish() {
+    let docTitle = document.title;
+
+    switch (docTitle) {
+        case "Startpage":
+
+            break;
+        case "Restaurant Finder":
+
+            break;
+
+        default:
+            break;
+    }
+}
+
+function makeLangEnglish() {
+    let docTitle = document.title;
+
+    switch (docTitle) {
+        case "Startsida":
+
+            break;
+        case "Resturang Hittaren":
+
+            break;
+
+        default:
+            break;
+    }
+}
