e00096a214932dd22af3e39d3cc23e41b9873c7b
CalleAlexanderson <calle.alexanderson@telia.com>
2024-02-29 21:35:03 +0100

    headerlayout klar

--- src/js/api.js
+++ src/js/api.js
@@ -11,7 +11,7 @@ window.addEventListener('load', init);
 async function getAIQstats() {
     let input = "taj mahal";
 
-    let inputForFetch = input.replace(/\s/g, '+') 
+    let inputForFetch = input.replace(/\s/g, '+')
 
     try {
         const response = await fetch(`https://nominatim.openstreetmap.org/search?addressdetails=1&q=${inputForFetch}&format=jsonv2&limit=1`);
@@ -21,20 +21,20 @@ async function getAIQstats() {
     }
 
     if (cordsData != null) {
-        console.log(cordsData);
+        let lat = cordsData[0].lat;
+        let lon = cordsData[0].lon;
+        // const options = {
+        //     method: 'GET',
+        //     headers: { 'X-Api-Key': 'wrWkYLoRM98tlbLu0CgjjA==VxslqbabF3vOSqvJ' },
+        //     contentType: 'application/json',
+        // };
+
+        // fetch(`https://api.api-ninjas.com/v1/airquality?lat=${lat}&lon=${lon}`, options)
+        //     .then(response => response.json())
+        //     .then(response => console.log(response))
+        //     .catch(err => console.error(err));
     }
 
-    let lat = cordsData[0].lat;
-    let lon = cordsData[0].lon;
-    // const options = {
-    //     method: 'GET',
-    //     headers: { 'X-Api-Key': 'wrWkYLoRM98tlbLu0CgjjA==VxslqbabF3vOSqvJ' },
-    //     contentType: 'application/json',
-    // };
 
-    // fetch(`https://api.api-ninjas.com/v1/airquality?lat=${lat}&lon=${lon}`, options)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
 
 }
